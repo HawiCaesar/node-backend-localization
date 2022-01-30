863d07d8b572cd9f78646b36ea79c29655506eb2
HawiCaesar <brianhawi92@gmail.com>
2022-01-30 17:30:46 +0300

    intial commit

---
+++ app.js
@@ -0,0 +1,119 @@
+
+import express from "express";
+import bcrypt from "bcryptjs";
+import { connect } from "./config/database";
+import { User } from './model/user'
+import { auth } from "./middleware/auth";
+import { i18next, i18nextMiddleware } from './i18n'
+
+import dotenv from 'dotenv'
+dotenv.config()
+import jwt from 'jsonwebtoken'
+
+connect()
+const app = express();
+
+app.use(express.json());
+
+app.use(
+   i18nextMiddleware.handle(i18next, {
+        ignoreRoutes: ['/foo'] // or function(req, res, options, i18next) { /* return true to ignore */ }
+    })
+)
+
+// Logic goes here
+
+
+// Register
+app.post("/register", async (req, res) => {
+// our register logic goes here...
+try {
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      return res.status(400).send(req.t('register.allRequired'));
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email: email.toLowerCase() });
+
+
+    if (oldUser) {
+      return res.status(409).send(req.t('register.existingUser'));
+    }
+
+    //Encrypt user password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    console.log(user, '&&&&&&')
+
+    // Create token
+    const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+      // save user token
+      user.token = token;
+  
+      // return new user
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err);
+    }
+});
+
+// Login
+app.post("/login", async (req, res) => {
+// our login logic goes here
+try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send(req.t('register.allRequired'));
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+        // save user token
+        user.token = token;
+
+        // user
+        res.status(200).json(user);
+      }
+      res.status(400).send(req.t('login.invalid'));
+    } catch (err) {
+      console.log(err);
+    }
+});
+
+app.get("/welcome", auth, (req, res) => {
+    res.status(200).send(`${req.t('welcome')} 🙌 `);
+  });
+
+export default app
---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+
+const config = process.env;
+
+export const auth = (req, res, next) => {
+    const token =
+      req.body.token || req.query.token || req.headers["x-access-token"];
+
+  
+    if (!token) {
+      return res.status(403).send(req.t("token.required"));
+    }
+    try {
+      const decoded = jwt.verify(token, config.TOKEN_KEY);
+      req.user = decoded;
+    } catch (err) {
+      return res.status(401).send(req.t("token.invalid"));
+    }
+    return next();
+  };
