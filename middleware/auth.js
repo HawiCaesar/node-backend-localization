342689ad84ffc3be4daedec089354db0421f3736
HawiCaesar <brianhawi92@gmail.com>
2022-01-30 17:33:34 +0300

    remove line in auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,6 @@ const config = process.env;
 export const auth = (req, res, next) => {
     const token =
       req.body.token || req.query.token || req.headers["x-access-token"];
-
   
     if (!token) {
       return res.status(403).send(req.t("token.required"));
