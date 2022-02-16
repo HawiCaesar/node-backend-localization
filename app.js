015c36853c7e6e4745166d69dd8c81e4649e100b
HawiCaesar <brianhawi92@gmail.com>
2022-02-16 19:27:08 +0300

    clean up content

--- app.js
+++ app.js
@@ -23,16 +23,16 @@ app.use(
 
 app.use((req, res, next) => {
   
-  // Website you wish to allow to connect
+  // domains to allow
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // Request methods you wish to allow
+  // Request methods to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
+  // Request headers to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token');
 
-  // Set to true if you need the website to include cookies in the requests sent
+  // Set to true if you need the domain to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
